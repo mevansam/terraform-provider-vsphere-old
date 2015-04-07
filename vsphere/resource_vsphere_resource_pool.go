5ec24e0238b09d5d8a51c52c0129d0f4a6d8b77f
Mevan Samaratunga <mevansam@gmail.com>
2015-04-07 00:04:53 -0400

    completed resource pool implementation

--- vsphere/resource_vsphere_resource_pool.go
+++ vsphere/resource_vsphere_resource_pool.go
@@ -1,10 +1,18 @@
 package vsphere
 
 import (
-//	"fmt"
+	"fmt"
+	"log"
+	"strings"
+	"strconv"
 	
-//	"golang.org/x/net/context"
+	"golang.org/x/net/context"
+	
 	"github.com/hashicorp/terraform/helper/schema"
+	"github.com/vmware/govmomi/find"
+	"github.com/vmware/govmomi/object"
+	"github.com/vmware/govmomi/vim25/mo"
+	"github.com/vmware/govmomi/vim25/types"
 )
 
 func resourceVsphereResourcePool() *schema.Resource {
@@ -18,22 +26,303 @@ func resourceVsphereResourcePool() *schema.Resource {
 
 		Schema: map[string]*schema.Schema{
 			
+			"name": &schema.Schema{
+				Type: schema.TypeString,
+				Required: true,
+			},			
+			"datacenter_id": &schema.Schema{
+				Type: schema.TypeString,
+				Required: true,
+			},
+			"parent_id": &schema.Schema{
+				Type: schema.TypeString,
+				Required: true,
+			},
+			"cpu": &schema.Schema{
+				Type: schema.TypeList,
+				Optional: true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"shares": &schema.Schema{
+							Type: schema.TypeString, // One of low, normal, high or an integer value for custom level
+							Optional: true,
+						},
+						"reservation": &schema.Schema{
+							Type: schema.TypeInt,
+							Optional: true,
+						},
+						"expandable_reservation": &schema.Schema{
+							Type: schema.TypeBool,
+							Optional: true,
+						},
+						"limit": &schema.Schema{
+							Type: schema.TypeInt,
+							Optional: true,
+						},
+					},
+				},
+			},
+			"memory": &schema.Schema{
+				Type: schema.TypeList,
+				Optional: true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"shares": &schema.Schema{
+							Type: schema.TypeString, // One of low, normal, high or an integer value for custom level
+							Optional: true,
+						},
+						"reservation": &schema.Schema{
+							Type: schema.TypeInt,
+							Optional: true,
+						},
+						"expandable_reservation": &schema.Schema{
+							Type: schema.TypeBool,
+							Optional: true,
+						},
+						"limit": &schema.Schema{
+							Type: schema.TypeInt,
+							Optional: true,
+						},
+					},
+				},
+			},
+			"keep": &schema.Schema{
+				Type: schema.TypeBool,
+				Optional: true,
+			},			
 		},
 	}
 }
 
 func resourceVsphereResourcePoolCreate(d *schema.ResourceData, meta interface{}) error {
+
+	var err error
+
+	_, err = findResourcePool(d, meta)
+	if err != nil {
+		
+		if err.Error() == fmt.Sprintf("resource pool %s was not found", d.Get("name").(string)) {
+			
+			finder, _, err := getFinder(d, meta)
+			if err != nil {
+				log.Printf("[ERROR] Unable to create finder for operations on resource pool: '%s'", d.Get("name").(string))
+				return err
+			}
+			
+			parentResourcePool, err := finder.ResourcePool(
+				context.Background(), fmt.Sprintf("/%s/*/%s/Resources", d.Get("datacenter_id").(string), d.Get("parent_id").(string)))
+			if err != nil {
+				log.Printf("[ERROR] Unable to retrieve default resource pool of parent '%s'", d.Get("parent_id").(string))
+				return err
+			}
+			
+			log.Printf("[DEBUG] Creating new resource pool at %s", parentResourcePool.InventoryPath)
+			
+			spec := types.ResourceConfigSpec{}
+			err = getAllocationInfo("cpu", &spec.CpuAllocation, d)
+			if err != nil {
+				return err
+			}
+			err = getAllocationInfo("memory", &spec.MemoryAllocation, d)
+			if err != nil {
+				return err
+			}
+			
+			_, err = parentResourcePool.Create(context.Background(), d.Get("name").(string), spec)
+			if err != nil {
+				log.Printf("[ERROR] Unable to create resource pool '%s'", d.Get("name").(string))
+				return err				
+			}
+			
+		} else {
+			return err
+		}
+	}
+	
+	d.SetId(d.Get("name").(string))
 	return nil
 }
 
 func resourceVsphereResourcePoolRead(d *schema.ResourceData, meta interface{}) error {
+	
+	resourcePool, err := findResourcePool(d, meta)
+	if err != nil {
+		d.SetId("")
+		return err
+	}
+	
+	log.Printf("[DEBUG] Reading configuration of resource pool at %s", resourcePool.InventoryPath)
+	
+	var mrp mo.ResourcePool
+	
+	ps := []string{"config"}
+	err = resourcePool.Properties(context.Background(), resourcePool.Reference(), ps, &mrp)
+	if err != nil {
+		return err
+	}
+	
+	putAllocationInfo("cpu", &mrp.Config.CpuAllocation, d)
+	putAllocationInfo("memory", &mrp.Config.CpuAllocation, d)
+	
 	return nil
 }
 
 func resourceVsphereResourcePoolUpdate(d *schema.ResourceData, meta interface{}) error {
+
+	resourcePool, err := findResourcePool(d, meta)
+	if err != nil {
+		d.SetId("")
+		return err
+	}
+	
+	log.Printf("[DEBUG] Updating resource pool at %s", resourcePool.InventoryPath)
+	
+	spec := types.ResourceConfigSpec{}
+	err = getAllocationInfo("cpu", &spec.CpuAllocation, d)
+	if err != nil {
+		return err
+	}
+	err = getAllocationInfo("memory", &spec.MemoryAllocation, d)
+	if err != nil {
+		return err
+	}
+	
+	err = resourcePool.UpdateConfig(context.Background(), d.Get("name").(string), &spec)
+	if err != nil {
+		log.Printf("[ERROR] Unable to update resource pool '%s'", d.Get("name").(string))
+		return err				
+	}
+	
 	return nil
 }
 
 func resourceVsphereResourcePoolDelete(d *schema.ResourceData, meta interface{}) error {
+
+	if keep, ok := d.GetOk("keep"); !ok || !keep.(bool) {		
+
+		resourcePool, err := findResourcePool(d, meta)
+		if err != nil {
+			return err
+		}
+		
+		log.Printf("[DEBUG] Deleting resource pool: %s", d.Id())
+		
+		task, err := resourcePool.Destroy(context.Background())
+		if err != nil {
+			return err
+		}
+	  	err = task.Wait(context.Background())
+		if err != nil {
+			return err
+		}		
+	}
 	return nil
+}
+
+func findResourcePool(d *schema.ResourceData, meta interface{}) (*object.ResourcePool, error) {
+	
+	var err error
+	
+	finder, _, err := getFinder(d, meta)
+	if err != nil {
+		log.Printf("[ERROR] Unable to create finder for operations on resource pool: '%s'", d.Get("name").(string))
+		return nil, err
+	}
+	
+	resourcePool, err := getResourcePool(d.Get("name").(string), d.Get("parent_id").(string), finder)
+	if err != nil {
+		return nil, err
+	}
+	
+	return resourcePool, nil
+}
+
+func getResourcePool(name string, parent string, finder *find.Finder) (*object.ResourcePool, error) {
+	
+	path := fmt.Sprintf("/Resources/%s", name)
+	matchPath := fmt.Sprintf("/%s%s", parent, path)
+	searchPath := fmt.Sprintf("*%s", path)
+	
+	resourcePoolList, err := finder.ResourcePoolList(context.Background(), searchPath)
+	if err != nil {
+		log.Printf("[ERROR] VMOMI error when searching for resource pool at path '%s'", searchPath)
+		return nil, err
+	}
+
+	for _, rp := range resourcePoolList {
+		
+		if strings.HasSuffix(rp.InventoryPath, matchPath) {
+			return rp, nil
+		}
+	}
+	
+	return nil, fmt.Errorf("resource pool %s was not found", name)
+}
+
+func getAllocationInfo(allocType string, allocInfo *types.ResourceAllocationInfo, d *schema.ResourceData) error {
+
+	var err error
+
+	v, ok := d.GetOk(fmt.Sprintf("%s.#", allocType))
+	if ok {
+		count := v.(int)
+		if count > 1 {
+			return fmt.Errorf("only 1 %s allocation section permitted", allocType)
+		}
+		if count == 1 {
+					
+			if v, ok := d.GetOk(fmt.Sprintf("%s.0.shares", allocType)); ok {
+				shares := types.SharesInfo{}
+				level := types.SharesLevel(v.(string))
+				if level != types.SharesLevelLow && 
+					level != types.SharesLevelNormal && 
+					level != types.SharesLevelHigh {
+						
+					shares.Shares, err = strconv.Atoi(v.(string))
+					if err != nil {
+						return fmt.Errorf("error converting customer share value to int: %s", err.Error())
+					}
+					shares.Level = types.SharesLevelCustom
+				} else {
+					shares.Level = level
+				}
+				allocInfo.Shares = &shares
+			} else {
+				allocInfo.Shares = &types.SharesInfo{
+					Level: types.SharesLevelNormal,
+				}
+			}
+			if v, ok := d.GetOk(fmt.Sprintf("%s.0.reservation", allocType)); ok {
+				allocInfo.Reservation = int64(v.(int))
+			}
+			if v, ok := d.GetOk(fmt.Sprintf("%s.0.expandable_reservation", allocType)); ok {
+				allocInfo.ExpandableReservation = v.(bool)
+			}
+			if v, ok := d.GetOk(fmt.Sprintf("%s.0.limit", allocType)); ok {
+				allocInfo.Limit = int64(v.(int))
+			}
+		}
+	}
+	return nil
+}
+
+func putAllocationInfo(allocType string, allocInfo *types.ResourceAllocationInfo, d *schema.ResourceData) {
+	
+	configState := make(map[string]interface{})
+	
+	if allocInfo.Reservation != 0 {
+		configState["reservation"] = strconv.FormatInt(allocInfo.Reservation, 10)
+	}
+	if allocInfo.Limit != 0 {
+		configState["limit"] = strconv.FormatInt(allocInfo.Limit, 10)
+	}
+	configState["expandable_reservation"] = strconv.FormatBool(allocInfo.ExpandableReservation)
+	
+	if allocInfo.Shares.Level == types.SharesLevelCustom {
+		configState["shares"] = strconv.Itoa(allocInfo.Shares.Shares)
+	} else {
+		configState["shares"] = string(allocInfo.Shares.Level)
+	}
+	
+	d.Set(allocType, append(make([]map[string]interface{}, 0, 1), configState))
 }
--- vsphere/resource_vsphere_resource_pool_test.go
+++ vsphere/resource_vsphere_resource_pool_test.go
@@ -1,26 +1,233 @@
 package vsphere
 
 import (
-//	"fmt"
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+	"runtime"
+	"strconv"
 	"testing"
 
+	"golang.org/x/net/context"
+	
 	"github.com/hashicorp/terraform/helper/resource"
-//	"github.com/hashicorp/terraform/helper/schema"
 	"github.com/hashicorp/terraform/terraform"
+	"github.com/kr/pretty"
+	"github.com/vmware/govmomi/object"
+	"github.com/vmware/govmomi/vim25/mo"
+	"github.com/vmware/govmomi/vim25/types"
 )
 
-func TestAccVsphereResourcePool_normal(t *testing.T) {
+var keepResourcePool bool
 
-	resource.Test( t, 
-		resource.TestCase {
-			PreCheck: func() { testAccPreCheck(t) },
-			Providers: testAccProviders,
-			CheckDestroy: testAccCheckResourcePoolDestroy,
-			Steps: []resource.TestStep {
+func TestAccVsphereResourcePool_normal(t *testing.T) {
+	
+	keepResourcePool = false
+
+	_, filename, _, _ := runtime.Caller(0)
+	ut := os.Getenv("UNIT_TEST")
+	if ut == "" || ut == filepath.Base(filename) {
+		
+		resource.Test( t, 
+			resource.TestCase {
+				PreCheck: func() { testAccPreCheck(t) },
+				Providers: testAccProviders,
+				CheckDestroy: testAccCheckResourcePoolDestroy,
+				Steps: []resource.TestStep {
+					resource.TestStep {
+						Config: testAccResourcePoolConfig,
+						Check: resource.ComposeTestCheckFunc(
+							testAccCheckResourcePoolExists("vsphere_resource_pool.rp1"),
+							resource.TestCheckResourceAttr("vsphere_resource_pool.rp1", "name", "resource_pool1"),
+							
+							resource.TestCheckResourceAttr(
+								"vsphere_resource_pool.rp1", "cpu.0.shares", "low"),
+							resource.TestCheckResourceAttr(
+								"vsphere_resource_pool.rp1", "cpu.0.reservation", "512"),
+							resource.TestCheckResourceAttr(
+								"vsphere_resource_pool.rp1", "cpu.0.expandable_reservation", "false"),
+							resource.TestCheckResourceAttr(
+								"vsphere_resource_pool.rp1", "cpu.0.limit", "1024"),
+							resource.TestCheckResourceAttr(
+								"vsphere_resource_pool.rp1", "memory.0.shares", "40960"),
+							resource.TestCheckResourceAttr(
+								"vsphere_resource_pool.rp1", "memory.0.reservation", "4096"),
+							resource.TestCheckResourceAttr(
+								"vsphere_resource_pool.rp1", "memory.0.expandable_reservation", "true"),
+							resource.TestCheckResourceAttr(
+								"vsphere_resource_pool.rp1", "memory.0.limit", "8192"),
+						),
+					},
 				},
-		} )
+			} )
+	}
+}
+
+func testAccCheckResourcePoolExists(resource string) resource.TestCheckFunc {
+	
+	return func(s *terraform.State) error {
+		
+		var err error
+
+		rs, ok := s.RootModule().Resources[resource]
+		if !ok {
+			return fmt.Errorf("resource pool '%s' not found in terraform state", resource)
+		}
+		
+		log.Printf("[DEBUG] Terraform resource pool: %# v", pretty.Formatter(rs))
+
+		attributes := rs.Primary.Attributes
+		resourcePoolName := rs.Primary.ID
+		
+		datacenterName := attributes["datacenter_id"]
+		parentName := attributes["parent_id"]
+		
+		resourcePool, err := findTestResourcePool(resourcePoolName, parentName, datacenterName)
+		if err != nil {
+			return err
+		}
+		
+		var mrp mo.ResourcePool
+		
+		ps := []string{"config"}
+		err = resourcePool.Properties(context.Background(), resourcePool.Reference(), ps, &mrp)
+		if err != nil {
+			return err
+		}
+
+		log.Printf("[DEBUG] Cluster config from read via VMOMI: %# v", pretty.Formatter(mrp.Config))
+		
+		if err := verifyResourceAllocation("cpu", mrp.Config.CpuAllocation, rs.Primary); err != nil {
+			return err		
+		}
+		if err := verifyResourceAllocation("memory", mrp.Config.MemoryAllocation, rs.Primary); err != nil {
+			return err		
+		}
+		
+		keepResourcePool = (attributes["keep"] == "true")
+		return nil
+	}
 }
 
 func testAccCheckResourcePoolDestroy(s *terraform.State) error {
+
+	const rp1 = "vsphere_resource_pool.rp1"
+	const datacenter3 = "datacenter3"
+	const cluster3 = "cluster3"
+	const resourcePool1 = "resource_pool1"
+
+	var(
+		ok bool
+		err error
+	)
+
+	_, ok = s.RootModule().Resources[rp1]
+	if ok {
+		return fmt.Errorf("resource pool '%s' still exists in the terraform state", rp1)
+	}
+
+	resourcePool, err := findTestResourcePool(resourcePool1, cluster3, datacenter3)
+	if err != nil {
+		log.Printf("[DEBUG] Resource pool '%s' destroyed as expected. API response was: %s", resourcePool1, err.Error())
+	} else if keepResourcePool {
+		log.Printf("[DEBUG] Resource pool '%s' not destroyed as expected", resourcePool.InventoryPath)
+	} else {
+		return fmt.Errorf("resource pool '%s' was not destroyed as expected", resourcePool.InventoryPath)
+	}
+	
 	return nil
 }
+
+func findTestResourcePool(resourcePoolName, parentName string, datacenterName string) (*object.ResourcePool, error) {
+	
+	finder, err := getTestFinder(datacenterName)
+	if err != nil {
+		return nil, err
+	}
+	
+	resourcePool, err := getResourcePool(resourcePoolName, parentName, finder)
+	if err != nil {
+		return nil, err
+	}
+	
+	return resourcePool, err
+}
+
+func verifyResourceAllocation(allocType string, allocInfo types.ResourceAllocationInfo, instance *terraform.InstanceState) error {
+	
+	attributes := instance.Attributes
+	num, err := strconv.Atoi(attributes[fmt.Sprintf("%s.#", allocType)])
+	if err != nil {
+		return err
+	}
+	
+	if num > 1 {
+		return fmt.Errorf("more than configuration section for resource allocation type for %s was found", allocType)
+	}
+	if num == 1 {
+		
+		if strconv.FormatInt(allocInfo.Reservation, 10) != attributes[fmt.Sprintf("%s.0.reservation", allocType)] {
+			fmt.Errorf("resource allocation %s reservation value mis-match", allocType)
+		}
+		if strconv.FormatBool(allocInfo.ExpandableReservation) != attributes[fmt.Sprintf("%s.0.expandable_reservation", allocType)] {
+			fmt.Errorf("resource allocation %s expandable reservation value mis-match", allocType)
+		}
+		if strconv.FormatInt(allocInfo.Limit, 10) != attributes[fmt.Sprintf("%s.0.limit", allocType)] {
+			fmt.Errorf("resource allocation %s limit value mis-match", allocType)
+		}
+		
+		v := attributes[fmt.Sprintf("%s.0.shares", allocType)]
+		level := types.SharesLevel(v)
+		if level != types.SharesLevelLow && 
+			level != types.SharesLevelNormal && 
+			level != types.SharesLevelHigh &&
+			(level != types.SharesLevelCustom || strconv.Itoa(allocInfo.Shares.Shares) != v) {
+		
+			fmt.Errorf("resource allocation %s shares structure mis-match", allocType)
+		}
+	}
+	
+	return nil
+} 
+
+const testAccResourcePoolConfig = `
+
+resource "vsphere_datacenter" "dc3" {
+	name = "datacenter3"
+
+	keep = true
+}
+
+resource "vsphere_cluster" "c3" {
+	name = "cluster3"
+	datacenter_id = "${vsphere_datacenter.dc3.id}"
+  
+	drs {}
+	ha {}
+
+	keep = true
+}
+
+resource "vsphere_resource_pool" "rp1" {
+	name = "resource_pool1"
+	datacenter_id = "${vsphere_datacenter.dc3.id}"
+	parent_id = "${vsphere_cluster.c3.id}"
+	
+	cpu {
+		shares = "low"
+		reservation = 512
+		expandable_reservation = false
+		limit = 1024
+	}
+	
+	memory {
+		shares = "40960"
+		reservation = 4096
+		expandable_reservation = true
+		limit = 8192
+	}
+	
+	keep = false
+}
+`
